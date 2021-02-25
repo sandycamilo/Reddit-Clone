a710b0ec015cfd69a5cedcb6405eafcd46473dea
sandycamilo <39415553+sandycamilo@users.noreply.github.com>
2021-02-25 00:24:59 -0800

    Users can submit and save posts

--- server.js
+++ server.js
@@ -1,23 +1,47 @@
+// Body parser 
+const bodyParser = require('body-parser');
+const expressValidator = require('express-validator');
+
 // Require Libraries
 const express = require('express')
 
 // App Setup
 const app = express()
 const port = 3000
+
+// middleware
+const exphbs = require('express-handlebars')
+
+// new post 
+require('./controllers/posts.js')(app);
+
+// mongodb NoSQL database 
+// Set db
+require('./data/reddit-db');
+
+// Use Body Parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.use(express.static('public'));
+
+// Add after body parser initialization!
+app.use(expressValidator());
 
 // Middleware 
 // "middleware" library will be used to allow Express (our web framework) to 
 // render HTML templates and send them back to the client using a new function: 
 // res.render('template-name', { variables })
-const exphbs = require('express-handlebars')
-
 app.engine('handlebars', exphbs({defaultLayout: 'main'}))
 app.set('view engine', 'handlebars')
 
 // Routes
+// home 
 app.get('/', (req, res) => {
   res.render('home')
+})
+app.get('/posts/new', (req, res) => {
+  res.render('posts-new')
 })
 
 // Start the server on port 3000
